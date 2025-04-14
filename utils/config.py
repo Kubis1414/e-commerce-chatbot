dbfbec95c92120d7d739f5892c9e81bd8657a250
Martin Kubánek <martin.kubanek@alza.cz>
2025-04-14 13:22:00 +0200

    Aktualizace modelů XAI v konfiguračním souboru: změna na 'grok-3-beta' a 'grok-3-mini-beta' pro zajištění aktuálnosti a dostupnosti modelů.

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ ANTHROPIC_BASIC_MODEL="claude-3-5-haiku-latest"
 GOOGLE_MODEL="gemini-2.0-flash"
 GOOGLE_BASIC_MODEL="gemini-2.0-flash-lite"
 
-XAI_MODEL="grok-2-latest"
-XAI_BASIC_MODEL="grok-2-latest" # XAI momentálně nemá žádný menší model, grok-3-mini není přes API momentánlě k dispozici
+XAI_MODEL="grok-3-beta"
+XAI_BASIC_MODEL="grok-3-mini-beta"
 
 WEAVIATE_URL="localhost"
